581c2d0dc478120c035dc5f652d56b162f7257a5
fujimu-88 <123345926+fujimu-88@users.noreply.github.com>
2023-01-24 00:25:12 +0900

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'azuyalabs/yasumi' => array(
+            'pretty_version' => '2.4.0',
+            'version' => '2.4.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../azuyalabs/yasumi',
+            'aliases' => array(),
+            'reference' => '083a0d0579fee17e68d688d463bc01098ac2691f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
